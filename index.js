2b29aa7c78d5aeb5cf4fe72b3888dab2bbf5f105
浪漫不属意 <1391936847@qq.com>
2019-04-14 17:04:20 +0800

    feat: 优化删除没用的东西，修改残留物

--- index.js
+++ index.js
@@ -42,9 +42,6 @@ app.use(allowCrossDomain);
  * set Routes
  */
 app.get('/', routes.index);
-app.get('/signup', routes.signup);
-app.get('/login', routes.login);
-app.get('/order', routes.order);
 app.get('/admin', require('./routes/admin/index').index);
 app.get('/admin/login', require('./routes/admin/login').login);
 app.get('/admin/register', require('./routes/admin/login').register);
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,3 @@
 exports.index = function (req, res) {
     res.redirect('/admin/')
 }
-
-exports.signup = function(req, res){
-    res.render("signup", {title: '注册', layout: 'base' });
-}
-
-exports.login = function(req, res){
-    res.render("login", {title: '登入' });
-}
-
-exports.order = function(req, res){
-    res.render("order", {title: '选座' });
-}
